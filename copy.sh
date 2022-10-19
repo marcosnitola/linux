ee8979474d86b83807b3f497572a2566ee91b858
marcosraccoon <marcos123.mdnr@gmail.com>
2022-10-19 09:33:31 -0500

    i3 files added

--- copy.sh
+++ copy.sh
@@ -3,5 +3,6 @@
 cp ~/.vimrc .
 cp ~/.bashrc .
 cp ~/.config/qtile/config.py ./.config/qtile/
+cp ~/.config/i3/* ./.config/i3/
 cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/alacritty.yml
 cp ~/.config/nvim/init.vim ./.config/nvim/init.vim
