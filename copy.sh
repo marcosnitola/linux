544d5496711ac0d7c6d84aa8c188f2539e379d9e
marcosraccoon <marcos123.mdnr@gmail.com>
2022-12-14 11:01:43 -0500

    Script copy improved

--- copy.sh
+++ copy.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 # Copies all the files from ~ to this repo
-cp ~/.vimrc .
-cp ~/.bashrc .
-cp ~/.config/qtile/config.py ./.config/qtile/
-cp ~/.config/i3/* ./.config/i3/
-cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/alacritty.yml
-cp ~/.config/nvim/init.vim ./.config/nvim/init.vim
+if [ "$1" = ".vimrc" ]; then
+  cp -v ~/.vimrc .
+elif [ "$1" = "" ]; then
+  cp -v ~/.vimrc .
+  cp -v ~/.bashrc .
+  cp -v ~/.config/qtile/config.py ./.config/qtile/
+  cp -v ~/.config/i3/* ./.config/i3/
+  cp -v ~/.config/alacritty/alacritty.yml ./.config/alacritty/alacritty.yml
+  cp -v ~/.config/nvim/init.vim ./.config/nvim/init.vim
+else
+  echo "Can't recognize $1"
+fi
