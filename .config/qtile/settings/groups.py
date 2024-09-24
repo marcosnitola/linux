9de3e712f30ad28c20e721470f4be0fd1ee17a3d
marcosnitola <marcos123.mdnr@gmail.com>
2024-09-23 23:01:47 -0500

    Qtile: Rename group names to icons

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -3,20 +3,26 @@ from libqtile.lazy import lazy
 from .keys import keys, mod
 
 groups = [
-    Group("NET", layout="max", matches=[
-        Match(wm_class="google-chrome"),
-        Match(wm_class="firefox"),
-        Match(wm_class="chromium"),
-        Match(wm_class="midori")]),
-    Group("TERM"),
-    Group("DEV"),
-    Group("SYS", matches=[Match(wm_class="thunar")]),
-    Group("CHAT", matches=[Match(wm_class="telegram-desktop")]),
-    Group("MEDIA", matches=[Match(wm_class="zoom"),Match(wm_class="vlc")]),
-    Group("GFX", layout="floating"),
-    Group("8"),
-    Group("9"),
-    Group("0")
+    Group("NET",   label=" 󰖟 ",
+        layout="max",
+        matches=[
+            Match(wm_class="google-chrome"),
+            Match(wm_class="firefox"),
+            Match(wm_class="chromium"),
+            Match(wm_class="midori")]),
+    Group("TERM",  label="  "),
+    Group("DEV",   label=" 󰅩 "),
+    Group("SYS",   label=" 󰣖 "),
+    Group("FILE",  label=" 󰉋 ",
+        matches=[Match(wm_class="thunar")]),
+    Group("CHAT",  label=" 󰊌 ",
+        matches=[Match(wm_class="telegram-desktop")]),
+    Group("MEDIA", label=" 󰎁 ",
+        matches=[Match(wm_class="zoom"),Match(wm_class="vlc")]),
+    Group("GFX",   label=" 󰘔 ",
+        layout="floating"),
+    Group("MISC",  label=" 󰛄 "),
+    Group("MISC2", label=" 󰛄 ")
 ]
 
 for i, group in zip(["1","2","3","4","5","6","7","8","9","0"], groups):
